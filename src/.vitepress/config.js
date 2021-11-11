96127f546c5f9b08b0d1dfdf610e12cf514f459f
Harold Kim <root@stypr.com>
2021-11-12 00:42:08 +0900

    Add swap

--- src/.vitepress/config.js
+++ src/.vitepress/config.js
@@ -12,9 +12,9 @@ module.exports = {
         ['meta', { name: 'referrer', content: 'no-referrer' }],
         ['link', { rel: 'shortcut icon', href: '/favicon.ico', type: 'image/x-icon' }],
         ['link', { rel: 'apple-touch-icon-precomposed', href: '/favicon.ico' }],
-        ['link', { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Lato:300' }],
-        ['link', { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Noto+Sans+KR:300' }],
-        ['link', { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Oxygen+Mono:400' }],
+        ['link', { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Lato:300&display=swap' }],
+        ['link', { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Noto+Sans+KR:300&display=swap' }],
+        ['link', { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Oxygen+Mono:400&display=swap' }],
         ['link', { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css' }]
     ],
     markdown: {
